a0926082e5e6d3f7a6d9773b3125cdd479305f10
Machiel <machielvanderwalt27@gmail.com>
2023-11-03 20:58:11 +0200

    Added framer motion animations

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,11 +1,12 @@
-// import React from "react";
 //import { Link } from "react-router-dom";
 import meImage from "./assets/me.jpg";
 import whatnowImage from "./assets/whatnow.png";
 import somniumImage from "./assets/somnium.png";
-import visorImage from "./assets/visor.png"
-import boredImage from "./assets/bored.png"
+import visorImage from "./assets/visor.png";
+import boredImage from "./assets/bored.png";
 import rpsImage from "./assets/rock-paper-scissors.png";
+
+import { motion } from "framer-motion";
 
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
@@ -15,9 +16,7 @@ function Portfolio() {
       {/* Navbar */}
       <nav className="bg-white p-6 shadow-md">
         <div className="container mx-auto flex justify-between items-center">
-          <div className="text-2xl font-bold hover:animate-bounce">
-            Machiel
-          </div>
+          <div className="text-2xl font-bold hover:animate-bounce">Machiel</div>
           <div className="flex items-center">
             {/* <Link to="/under-construction" className="mx-4">
               Projects
@@ -93,37 +92,42 @@ function Portfolio() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
             {/* Single Project */}
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <a
-                href="https://whatnow-three.vercel.app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={whatnowImage}
-                  alt="whatnow"
-                  className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
-                />
-              </a>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                <a
+                  href="https://whatnow-three.vercel.app"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={whatnowImage}
+                    alt="whatnow"
+                    className="mb-4 rounded-lg"
+                  />
+                </a>
+              </motion.div>
               <h3 className="text-xl font-bold mb-4">
                 Product Ordering System
               </h3>
               <p>
-                Please note this project is no longer being maintained. A basic CRUD application built with MSSQL Server, ASP.NET Core,
-                React JS and Bootstrap.
+                Please note this project is no longer being maintained. A basic
+                CRUD application built with MSSQL Server, ASP.NET Core, React JS
+                and Bootstrap.
               </p>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <a
-                href="https://machiel27.github.io/landing-page/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={somniumImage}
-                  alt="somnium"
-                  className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
-                />
-              </a>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                <a
+                  href="https://machiel27.github.io/landing-page/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={somniumImage}
+                    alt="somnium"
+                    className="mb-4 rounded-lg"
+                  />
+                </a>
+              </motion.div>
               <h3 className="text-xl font-bold mb-4">Flexbox Landing Page</h3>
               <p>
                 A simple, static Landing Page for a fictional company. Used to
@@ -131,53 +135,87 @@ function Portfolio() {
               </p>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <a
-                href="https://machiel27.github.io/rock-paper-scissors/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={rpsImage}
-                  alt="rock-paper-scissors"
-                  className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
-                />
-              </a>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                <a
+                  href="https://machiel27.github.io/rock-paper-scissors/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={rpsImage}
+                    alt="rock-paper-scissors"
+                    className="mb-4 rounded-lg"
+                  />
+                </a>
+              </motion.div>
               <h3 className="text-xl font-bold mb-4">Rock, Paper, Scissors</h3>
               <p>A Vanilla JavaScript game I created for the Odin Project.</p>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <a
-                href="https://github.com/machiel27/visor"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={visorImage}
-                  alt="visor"
-                  className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
-                />
-              </a>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                <a
+                  href="https://github.com/machiel27/visor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={visorImage}
+                    alt="visor"
+                    className="mb-4 rounded-lg"
+                  />
+                </a>
+              </motion.div>
               <h3 className="text-xl font-bold mb-4">Visor</h3>
-              <p>A Dashboard for viewing F1 stats. Built with Angular, Apache ECharts, .NET API, and MSSQL Database.</p>
+              <p>
+                A Dashboard for viewing F1 stats. Built with Angular, Apache
+                ECharts, .NET API, and MSSQL Database.
+              </p>
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <a
-                href="https://lowdefy-bored.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src={boredImage}
-                  alt="boredom-fighter"
-                  className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
-                />
-              </a>
+              <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }}>
+                <a
+                  href="https://lowdefy-bored.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={boredImage}
+                    alt="boredom-fighter"
+                    className="mb-4 rounded-lg"
+                  />
+                </a>
+              </motion.div>
               <h3 className="text-xl font-bold mb-4">Boredom-Fighter</h3>
-              <p>An application that generates ideas and adds them to a to-do list. Built with Lowdefy</p>
+              <p>
+                An application that generates ideas and adds them to a to-do
+                list. Built with Lowdefy
+              </p>
             </div>
           </div>
         </div>
       </section>
+
+      {/* Animation Test */}
+      {/* <div className="bg-white p-6 m-10">
+        <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+          <a
+            href="https://lowdefy-bored.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={boredImage}
+              alt="boredom-fighter"
+              className="mb-4 rounded-lg transition-transform duration-500 ease-in-out transform hover:rotate-12"
+            />
+          </a>
+          <h3 className="text-xl font-bold mb-4">Boredom-Fighter</h3>
+          <p>
+            An application that generates ideas and adds them to a to-do list.
+            Built with Lowdefy
+          </p>
+        </motion.div>
+      </div> */}
 
       {/* Footer */}
       <footer className="bg-white p-6 mt-10">
